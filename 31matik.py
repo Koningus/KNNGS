44fbfabf77de9e6536ece7f214a72086929b57a1
Koningus <89037577+Koningus@users.noreply.github.com>
2021-08-23 19:58:41 +0300

    Komik sayıya gülen python kod
    çokomik

---
+++ 31matik.py
@@ -0,0 +1,21 @@
+print ("""************************************
+
+31 Matik                
+
+Programdan çıkmak için 'q'
+
+************************************""")
+
+while True:
+    işlem = input("Sayı Giriniz:")
+
+    if (işlem  ==  "q"):
+        print ("Görşrx byz")
+        break
+
+    if (işlem  ==  "31"  ) :
+        print ("SAKLŞFJSDKLJFKLASDJFLKADF ÇOKOMİK LAN")
+        
+
+    else:
+        print("Kanka 31 yaz")
